de6ef55c12f22e77dd44b393821bf16a0130ad07
satya889 <36898014+satya889@users.noreply.github.com>
2023-03-13 21:55:53 +0530

    5 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,11 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!' + products[0].name);
   }
+  onNotify() {
+    window.alert(
+      'Notification done' 
+    );
+  }
 }
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js';
+import 'zone.js/dist/long-stack-trace-zone.js';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
 /*
 Copyright Google LLC. All Rights Reserved.
